df6709889550a0760c025d35140427cdcd447274
Jay Prakash <jay5222347@gmail.com>
2025-01-24 18:08:27 +0530

    initial done

---
+++ config.js
@@ -0,0 +1,3 @@
+module.exports={
+    JWT_SECREST:"mynameisjay@789974#"
+}
---
+++ routes/index.js
@@ -0,0 +1,5 @@
+const express=require('express');
+const userrouter=require('/user')
+const router=express.Router();
+router.use("/user",userrouter);
+module.exports=router;
